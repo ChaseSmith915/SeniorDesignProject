149705fe69970680bdd6fd1c6236328c174b9011
ChaseSmith915 <82478282+ChaseSmith915@users.noreply.github.com>
2025-10-06 17:28:03 -0400

    Initial commit

---
+++ SeniorDesignProject/SeniorDesignProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SeniorDesignProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SeniorDesignProject/SeniorDesignProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SeniorDesignProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
