74c01001bc2f232a203571cb86621759bdb098dc
Nicholas Dumford <nicholas32k4@gmail.com>
2025-11-11 00:20:14 -0500

    Starting to format main page like wireframe.

--- MauiApp1_testing_android_fesability/MauiApp1_testing_android_fesability/MainPage.xaml.cs
+++ MauiApp1_testing_android_fesability/MauiApp1_testing_android_fesability/MainPage.xaml.cs
@@ -47,16 +47,5 @@ namespace MauiApp1_testing_android_fesability
             };
 #endif
         }
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 }
