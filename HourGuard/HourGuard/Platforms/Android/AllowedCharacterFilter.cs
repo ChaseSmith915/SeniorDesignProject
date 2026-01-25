a1430329426616083b1f1ee858b79e4a6331447d
fechtert <bobby.fechtel@gmail.com>
2026-01-25 17:16:23 -0500

    sanitized the input
    - added 16 character limit to input box
    - added allowed characters class (only allows letters, numbers, spaces)
    - enforces both restrictions on the input box

---
+++ HourGuard/HourGuard/Platforms/Android/AllowedCharacterFilter.cs
@@ -0,0 +1,27 @@
+﻿using Android.Text;
+using Java.Lang;
+
+public class AllowedCharacterFilter : Java.Lang.Object, IInputFilter
+{
+    public ICharSequence FilterFormatted(
+        ICharSequence source,
+        int start,
+        int end,
+        ISpanned dest,
+        int dstart,
+        int dend)
+    {
+        for (int i = start; i < end; i++)
+        {
+            char c = source.CharAt(i);
+
+            // Allow letters, digits, and spaces
+            if (!char.IsLetterOrDigit(c) && c != ' ')
+            {
+                return new Java.Lang.String(""); // block invalid characters
+            }
+        }
+
+        return null; // accept input
+    }
+}
