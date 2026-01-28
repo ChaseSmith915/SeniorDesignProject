63bb80a2a2195696b2beff19c83d90a0cc33872b
Nicholas Dumford <nicholas32k4@gmail.com>
2026-01-28 03:54:38 -0500

    Updated timer to only show warnings for daily timers

--- HourGuard/HourGuard/Platforms/Android/HourGuardTimer.cs
+++ HourGuard/HourGuard/Platforms/Android/HourGuardTimer.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace HourGuard.Platforms.Android
 {
-    class HourGuardTimer
+    internal class HourGuardTimer
     {
         // Timer status constants returned by TickTimers
         public const int TIMER_RUNNING = 0;
@@ -24,12 +24,21 @@ namespace HourGuard.Platforms.Android
         private TimeSpan sessionTimeLimit;
         private TimeSpan sessionTimeUsed;
 
+        // Constructors (Time limits <= 0 indicate no limit)
         public HourGuardTimer(TimeSpan dailyTimeLimit, TimeSpan dailyTimeUsed, TimeSpan sessionTimeLimit, TimeSpan sessionTimeUsed)
         {
             this.dailyTimeLimit = dailyTimeLimit;
             this.dailyTimeUsed = dailyTimeUsed;
             this.sessionTimeLimit = sessionTimeLimit;
             this.sessionTimeUsed = sessionTimeUsed;
+        }
+
+        public HourGuardTimer(TimeSpan dailyTimeLimit, TimeSpan sessionTimeLimit)
+        {
+            this.dailyTimeLimit = dailyTimeLimit;
+            this.dailyTimeUsed = TimeSpan.Zero;
+            this.sessionTimeLimit = sessionTimeLimit;
+            this.sessionTimeUsed = TimeSpan.Zero;
         }
 
         public TimeSpan GetDailyTimeLimit()
@@ -82,31 +91,62 @@ namespace HourGuard.Platforms.Android
          */
         public (int dailyTimerStatus, int sessionTimerStatus) TickTimers(TimeSpan timeElapsed)
         {
+            return (GetDailyTimerStatus(timeElapsed), GetSessionTimerStatus(timeElapsed));
+        }
+
+        /** Check the status of the daily timer after ticking by the specified elapsed time.
+         * 
+         * @param timeElapsed The time elapsed since the last tick.
+         * @return A tuple containing the status of the daily timer and session timer:
+         *         - 0: Timer is still running without issue
+         *         - 1: Timer is about to reach its limit (5 minutes)
+         *         - 2: Timer has reached its limit
+         */
+        private int GetDailyTimerStatus(TimeSpan timeElapsed)
+        {
             int dailyStatus = TIMER_RUNNING;
+
+            // If there is no daily time limit, never return warnings or exceeded status
+            if (dailyTimeLimit > TimeSpan.Zero)
+            {
+                this.dailyTimeUsed += timeElapsed;
+
+                if (dailyTimeLimit - dailyTimeUsed <= TimeSpan.Zero)
+                {
+                    dailyStatus = TIMER_EXCEEDED;
+                }
+                else if (dailyTimeLimit - dailyTimeUsed <= WARN_DURATION)
+                {
+                    dailyStatus = TIMER_WARNING;
+                }
+            }
+
+            return dailyStatus;
+        }
+
+        /** Check the status of the session timer after ticking it by the specified elapsed time.
+         * 
+         * @param timeElapsed The time elapsed since the last tick.
+         * @return A tuple containing the status of the daily timer and session timer:
+         *         - 0: Timer is still running without issue
+         *         - 2: Timer has reached its limit
+         */
+        private int GetSessionTimerStatus(TimeSpan timeElapsed)
+        {
             int sessionStatus = TIMER_RUNNING;
 
-            this.dailyTimeUsed += timeElapsed;
-            this.sessionTimeUsed += timeElapsed;
+            // If there is no session time limit, never return warnings or exceeded status
+            if (sessionTimeLimit > TimeSpan.Zero)
+            {
+                this.sessionTimeUsed += timeElapsed;
 
-            if (dailyTimeLimit - dailyTimeUsed <= TimeSpan.Zero)
-            {
-                dailyStatus = TIMER_EXCEEDED;
-            }
-            else if (dailyTimeLimit - dailyTimeUsed <= WARN_DURATION)
-            {
-                dailyStatus = TIMER_WARNING;
-            }
-
-            if (sessionTimeLimit - sessionTimeUsed <= TimeSpan.Zero)
-            {
-                sessionStatus = TIMER_EXCEEDED;
-            }
-            else if (sessionTimeLimit - sessionTimeUsed <= WARN_DURATION)
-            {
-                sessionStatus = TIMER_WARNING;
+                if (sessionTimeLimit - sessionTimeUsed <= TimeSpan.Zero)
+                {
+                    sessionStatus = TIMER_EXCEEDED;
+                }
             }
 
-            return (dailyStatus, sessionStatus);
+            return sessionStatus;
         }
     }
 }
