a92e128745d319304269c6b8cc54df23e3954dad
fechtert <bobby.fechtel@gmail.com>
2026-02-01 12:17:43 -0500

    created AndroidAppUtils class
    Moved GetAppNameFromPackage to its own class so it can be reused

---
+++ HourGuard/HourGuard/Platforms/Android/AndroidAppUtils.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content.PM;
+
+namespace HourGuard
+{
+    public static class AndroidAppUtils
+    {
+#if ANDROID
+        public static string GetAppNameFromPackage(string packageName)
+        {
+            string appName = "";
+            PackageManager pm = Android.App.Application.Context.PackageManager;
+
+            try
+            {
+                // Get metadata for the installed app
+                ApplicationInfo appInfo = pm.GetApplicationInfo(packageName, 0);
+
+                // Convert to readable label (e.g., "YouTube")
+                appName = pm.GetApplicationLabel(appInfo);
+            }
+            catch (PackageManager.NameNotFoundException)
+            {
+                // App was uninstalled or package name is invalid
+            }
+
+            return appName;
+        }
+#endif
+    }
+}
