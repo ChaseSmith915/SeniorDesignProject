f9c963fbe5378342e2fe6377afc5af879e998a5b
ChaseSmith915 <82478282+ChaseSmith915@users.noreply.github.com>
2026-02-04 19:52:10 -0500

    optimized targeted apps screen
    yay

---
+++ HourGuard/HourGuard/AppItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HourGuard
+{
+    public class AppItem
+    {
+        public string Name { get; set; }
+        public string PackageName { get; set; }
+        public ImageSource Icon { get; set; }
+    }
+}
